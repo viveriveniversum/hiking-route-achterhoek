c93b6e9a29e150820cf2c441f54f3eb6cbc48ab0
Onur <onurmanap2461@gmail.com>
2025-01-22 21:05:22 +0100

    export user model

--- models/User.js
+++ models/User.js
@@ -9,4 +9,5 @@ const UserSchema = new mongoose.Schema({
   updatedAt: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model("User", UserSchema);
+const User = mongoose.model("User", UserSchema);
+export default User;
