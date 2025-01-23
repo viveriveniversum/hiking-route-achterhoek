dcfe74b5acb8c67d39945686b82300a0ee17b934
Onur <onurmanap2461@gmail.com>
2025-01-23 23:16:45 +0100

    Map updates - fetch api update
    - some small styling

--- components/map/RouteMap.vue
+++ components/map/RouteMap.vue
@@ -17,7 +17,8 @@
           :key="index"
           :lat-lng="point"
         />
-        <LPolyline :lat-lngs="route" v-if="route.length" />
+        <LPolyline :lat-lngs="route" v-if="route.length" color="green" />
+        <!-- Achterhoek Green :D -->
       </LMap>
     </div>
   </div>
@@ -32,78 +33,77 @@ const loading = ref(true);
 const routeStore = useRouteStore();
 const { selectedRoute, routes } = storeToRefs(routeStore);
 
-// both routes and points data
-const [{ data: routesData }, { data: pointsData }] = await Promise.all([
-  useFetch("/api/routes"),
-  useFetch("/api/points"),
-]);
+const fetchRoute = async (points) => {
+  try {
+    const response = await fetch(import.meta.env.VITE_ORS_URL, {
+      method: "POST",
+      headers: {
+        Accept:
+          "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8",
+        "Content-Type": "application/json",
+        Authorization: import.meta.env.VITE_ROUTE_API_KEY,
+      },
+      body: JSON.stringify({
+        coordinates: points.map(([lat, lng]) => [lng, lat]),
+      }),
+    });
 
-if (pointsData.value && routesData.value) {
-  routeStore.setPoints(pointsData.value);
-  routeStore.setRoutesFromDB(routesData.value);
-}
-loading.value = false;
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    // ORS uses reverse lon/lat
+    route.value = data.features[0].geometry.coordinates.map(([lng, lat]) => [
+      lat,
+      lng,
+    ]);
+  } catch (error) {
+    console.error("Error fetching route:", error);
+  }
+};
+
+// Initializing the map, dummy points & routes
+const initializeData = async () => {
+  try {
+    const [{ data: routesData }, { data: pointsData }] = await Promise.all([
+      useFetch("/api/routes"),
+      useFetch("/api/points"),
+    ]);
+
+    if (pointsData.value && routesData.value) {
+      routeStore.setPoints(pointsData.value);
+      routeStore.setRoutesFromDB(routesData.value);
+    }
+    loading.value = false;
+  } catch (error) {
+    console.error("Error loading initial data:", error);
+    loading.value = false;
+  }
+};
 
 const center = ref([51.867094010160336, 6.485649003772237]);
 const zoom = ref(13);
-
-// Route data
 const route = ref([]);
 
-// Update center and points dynamically based on the selected route
 const selectedPoints = ref([]);
 
-const fetchRoute = (points) => {
-  const apiKey = import.meta.env.VITE_ROUTE_API_KEY;
-  const url =
-    "https://api.openrouteservice.org/v2/directions/foot-walking/geojson";
-
-  const request = new XMLHttpRequest();
-  request.open("POST", url);
-
-  request.setRequestHeader(
-    "Accept",
-    "application/json, application/geo+json, application/gpx+xml, img/png; charset=utf-8"
-  );
-  request.setRequestHeader("Content-Type", "application/json");
-  request.setRequestHeader("Authorization", apiKey);
-
-  request.onreadystatechange = function () {
-    if (this.readyState === 4) {
-      if (this.status === 200) {
-        try {
-          const data = JSON.parse(this.responseText);
-          route.value = data.features[0].geometry.coordinates.map(
-            ([lng, lat]) => [lat, lng]
-          );
-        } catch (error) {
-          console.error("Error parsing response:", error);
-        }
-      } else {
-        console.error("Error fetching route:", this.status, this.responseText);
-      }
+watch(
+  selectedRoute,
+  (newRoute) => {
+    if (newRoute && routes.value[newRoute]) {
+      const { center: newCenter, points } = routes.value[newRoute];
+      center.value = newCenter;
+      selectedPoints.value = points;
+      fetchRoute(points);
     }
-  };
+  },
+  { immediate: true } // necessary for inital load
+);
 
-  const body = JSON.stringify({
-    coordinates: points.map(([lat, lng]) => [lng, lat]),
-  });
+onMounted(async () => {
+  await initializeData();
 
-  request.send(body);
-};
-
-// Watch for changes in selectedRoute
-watch(selectedRoute, (newRoute) => {
-  if (newRoute && routes.value[newRoute]) {
-    const { center: newCenter, points } = routes.value[newRoute];
-    center.value = newCenter;
-    selectedPoints.value = points;
-    fetchRoute(points);
-  }
-});
-
-// Fetch initial route on mount
-onMounted(() => {
   if (routes.value[selectedRoute.value]) {
     const { center: initialCenter, points } = routes.value[selectedRoute.value];
     center.value = initialCenter;
